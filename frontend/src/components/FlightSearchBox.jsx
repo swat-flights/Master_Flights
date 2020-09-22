199d8006af8d8071c07e39762f67310c0b5fe256
Luis <lcaraballocc@gmail.com>
2020-09-22 13:27:53 -0500

    [ADD] Creates LandingPage

---
+++ frontend/src/components/FlightSearchBox.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './styles/FlightSearchBox.sass';
+
+const FlightSearchBox = () => (
+  <div className="SearchBox">
+    <div className="SearchBox__container">
+      <h3>Asi volamos en Sparrow, volar es enamorarse</h3>
+      <h5>Busca aca el vuelo que mas te convenga</h5>
+      <div>
+        <form action="" className="SearchBox__form">
+          <div className="SearchBox__form__item">
+            <label htmlFor="from">Origen</label>
+            <span className="material-icons">trip_origin</span>
+            <input id="from" type="text" placeholder="Ciudad Origen" />
+          </div>
+
+          <div className="SearchBox__form__item">
+            <label htmlFor="to">Destino</label>
+            <span className="material-icons">location_on</span>
+            <input id="to" type="text" placeholder="Ciudad Destino" />
+          </div>
+
+          <div className="SearchBox__form__item">
+            <label htmlFor="departure">Fecha Ida</label>
+            <span className="material-icons">today</span>
+            <input id="departure" type="text" placeholder="dd/mm/aa" />
+          </div>
+
+          <div className="SearchBox__form__item">
+            <label htmlFor="return">Fecha Regreso</label>
+            <span className="material-icons">today</span>
+            <input id="return" type="text" placeholder="dd/mm/aa" />
+          </div>
+          <div className="SearchBox__form__button">
+            <input type="button" value="Buscar" />
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+);
+
+export default FlightSearchBox;
---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './page/LandingPage';
+import './index.sass';
+
+ReactDOM.render(<App />, document.getElementById('app'));
