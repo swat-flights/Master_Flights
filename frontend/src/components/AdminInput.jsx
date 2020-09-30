e1b6947d7bf4b0e4b95d6c56039b2a2b13116b58
agustin <agustin.moranr@gmail.com>
2020-09-29 22:09:58 -0500

    [ADD] creation of AdminInput, a component for the forms

---
+++ frontend/src/components/AdminInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/AdminInput.sass';
+
+const AdminInput = () => {
+  return (
+    <div className="Labels">
+      <p>Lorem ipsum</p>
+      <input placeholder="Lorem ipsum" />
+    </div>
+  );
+};
+
+export default AdminInput;
--- frontend/src/page/AdminAddFlights.jsx
+++ frontend/src/page/AdminAddFlights.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import AdminHeader from '../components/AdminHeader';
 import AdminSidebar from '../components/AdminSidebar';
 import AdminFooter from '../components/AdminFooter';
+import AdminInput from '../components/AdminInput';
+import './styles/AdminAddFlights.sass';
 
 const AdminAddFlights = () => {
   return (
     <>
       <AdminHeader />
-      <AdminSidebar />
+      <div className="Forms">
+        <AdminSidebar />
+        <AdminInput />
+      </div>
       <AdminFooter />
     </>
   );
