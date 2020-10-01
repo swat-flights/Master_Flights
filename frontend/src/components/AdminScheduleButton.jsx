c82b2b9371d3d3496e9396b865d177f5352308a6
agustin <agustin.moranr@gmail.com>
2020-10-01 14:55:06 -0500

    [ADD] AdminAddFlights view completed

---
+++ frontend/src/components/AdminScheduleButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/AdminScheduleButton.sass';
+
+const AdminScheduleButton = () => {
+  return (
+    <div className="Select">
+      <input type="button" />
+      <p>Monday</p>
+    </div>
+  );
+};
+
+export default AdminScheduleButton;
--- frontend/src/page/AdminAddFlights.jsx
+++ frontend/src/page/AdminAddFlights.jsx
@@ -3,15 +3,57 @@ import AdminHeader from '../components/AdminHeader';
 import AdminSidebar from '../components/AdminSidebar';
 import AdminFooter from '../components/AdminFooter';
 import AdminInput from '../components/AdminInput';
+import AdminScheduleButton from '../components/AdminScheduleButton';
 import './styles/AdminAddFlights.sass';
 
 const AdminAddFlights = () => {
   return (
     <>
       <AdminHeader />
-      <div className="Forms">
+      <div className="Main">
         <AdminSidebar />
-        <AdminInput />
+        <div className="Forms">
+          <div className="Forms__data">
+            <h1>Información de Aerolinea</h1>
+            <div className="Forms__AirlineFields">
+              <AdminInput />
+              <AdminInput />
+            </div>
+          </div>
+          <div className="Forms__data">
+            <h1>Trayecto de Vuelo</h1>
+            <div className="Forms__JourneyFields">
+              <div className="duples">
+                <AdminInput />
+                <AdminInput />
+              </div>
+              <div className="duples2">
+                <AdminInput />
+                <AdminInput />
+              </div>
+            </div>
+          </div>
+          <div className="Forms__data">
+            <h1>Horario de Vuelo</h1>
+            <p> Días </p>
+            <ul>
+              <AdminScheduleButton />
+              <AdminScheduleButton />
+              <AdminScheduleButton />
+              <AdminScheduleButton />
+              <AdminScheduleButton />
+              <AdminScheduleButton />
+              <AdminScheduleButton />
+            </ul>
+            <div className="Forms__TimeButton">
+              <p>Lorem ipsum</p>
+              <input placeholder="Lorem ipsum" />
+            </div>
+          </div>
+          <button className="Forms__button" type="button">
+            CREAR VUELO
+          </button>
+        </div>
       </div>
       <AdminFooter />
     </>
