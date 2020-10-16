537a2ece85097cfc2a024d335d7efaa3d7d5d151
Agustin Moran <agustin.moranr@gmail.com>
2020-10-15 19:30:50 -0500

    [ADD] Buscador de vuelos del administrador

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './page/AdminEditFlights';
+import App from './page/AdminSearchDeleteFligths';
 import './index.sass';
 
 ReactDOM.render(<App />, document.getElementById('app'));
