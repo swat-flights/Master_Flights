67989a8cfbd7f9d50ab3cfc8cdef9615eb36e274
Agustin Moran <agustin.moranr@gmail.com>
2020-11-07 17:27:14 -0600

    [Fix] Adding button insted of ancor in sidebar component

--- frontend/src/components/AdminSidebarButton.jsx
+++ frontend/src/components/AdminSidebarButton.jsx
@@ -4,10 +4,16 @@ import './styles/AdminSidebarButton.sass';
 const AdminSidebarButton = ({ action }) => {
   return (
     <div className="Sidebar__newFly">
-      <a className="Sidebar__newFly-button" href="/">
+      <button
+        onClick={() => {
+          window.alert('lorem ipsum');
+        }}
+        className="Sidebar__newFly_button"
+        type="button"
+      >
         <span className="material-icons">add_circle_outline</span>
         {action}
-      </a>
+      </button>
     </div>
   );
 };
