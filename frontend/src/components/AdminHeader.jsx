bbf2bc1238e62968edf17aba6a601a6380598d87
Agustin Moran <agustin.moranr@gmail.com>
2020-11-07 16:48:52 -0600

    [Fix] responsivnes of header

--- frontend/src/components/AdminHeader.jsx
+++ frontend/src/components/AdminHeader.jsx
@@ -10,12 +10,14 @@ const AdminHeader = ({ headerTitle }) => {
           <img className="Header__logo-img" src={Logo} alt="logo" />
           <p className="Header__logo-title">ADMINISTRADOR</p>
         </div>
-        <h1 className="Header__title">{headerTitle}</h1>
+        <div className="Header__title">
+          <h1>{headerTitle}</h1>
+        </div>
         <div className="Header__userItems">
           <a className="Header__logout" href="/">
-            Cerrar Sesión
+            <div>Cerrar Sesión</div>
+            <span className="material-icons">account_circle</span>
           </a>
-          <span className="material-icons">account_circle</span>
         </div>
       </div>
       <div className="Header__highlighter" />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './page/AdminSearchDeleteFligths';
+import App from './page/AdminAddFlights';
 import './index.sass';
 
 ReactDOM.render(<App />, document.getElementById('app'));
