b685786ddee5751047d68c47512bc47d6437f78b
Lcaraballoc <luisred61@gmail.com>
2020-11-20 18:59:58 -0500

    [ADD] Starts redux implementation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { Router } from 'react-router';
 import { createBrowserHistory } from 'history';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './routes/App';
+import reducer from './reducers';
+import initialState from './initialState';
 import './index.sass';
 
+const store = createStore(reducer, initialState);
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={history}>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('app')
 );
---
+++ frontend/src/initialState.js
@@ -0,0 +1,7 @@
+const initialState = {
+  status: 'idle_status',
+  data: [],
+  error: null,
+};
+
+export default initialState;
